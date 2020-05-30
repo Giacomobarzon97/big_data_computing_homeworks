036c3d6af839c475f8892c010dff2af4f48a782c
gego <mazzalovo@live.it>
2020-05-30 15:23:22 +0200

    h3

--- H3/src/main/java/G48HW3.java
+++ H3/src/main/java/G48HW3.java
@@ -8,6 +8,7 @@
 //The k-center-based algorithm seems to be best compromise, it gets very good approximations with low execution times
 //even with small values of k
 
+import com.google.inject.internal.asm.$ByteVector;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -19,6 +20,8 @@ import scala.Tuple2;
 import java.io.IOException;
 import java.util.*;
 
+import static java.lang.Math.sqrt;
+
 public class G48HW3 {
 
     static long SEED=1231829;
@@ -27,70 +30,76 @@ public class G48HW3 {
     static final SparkConf conf = new SparkConf(true).setAppName("Homework3").setMaster("local").set("spark.testing.memory", "2147480000");
     static final  JavaSparkContext sc = new JavaSparkContext(conf);
 
-    private static JavaPairRDD<Vector, ArrayList<Vector>> farthestFirstTraversal(Iterator<Object> pointsRDD, int k){
-        List<Vector> S = new ArrayList<>();
-        List<Vector> points = new ArrayList<>();
-        pointsRDD.forEachRemaining(el -> {points.add((Vector)el);});
-        S.add( points.remove(generator.nextInt(points.size() - 1)));
-
-        Map<Vector, Tuple2<Vector, Double>> partition = new HashMap<>();
-
-        // Iterate k-1 times to find other centers
-        for(int i=0; i < k-1; i++) {
-            int max = -1;
-            double dist = -1;
-            // Find point pi in points that maximizes d(pi, S)
-            // Iterate all centers
-            for(int c = 0; c < S.size(); c++) {
-                // Iterate all points in P - S
-                Vector actCenter = S.get(c);
-                for (int p = 0; p < points.size(); p++) {
-                    Vector actPoint = points.get(p);
-                    double actDist = Vectors.sqdist(actCenter, actPoint);
-                    /*
-                    * Since in each iteration calculates the distance between each point and each center, keep trace
-                    * of closest center to each point instead of calling Partition(P, S) in the end.
-                    */
-                    if(!partition.containsKey(actPoint) || partition.get(actPoint)._2 > actDist )
-                        partition.put(actPoint, new Tuple2<Vector, Double>(actCenter, actDist));
-
-                    if ( actDist > dist ) {
-                        dist = actDist;
-                        max = p;
-                    }
+    //implementation of the kcenter algorithm
+    private static Iterator<Vector> farthestFirstTraversal(Iterator<Vector> pointsRDD, int k){
+        //Data structures initialization
+        ArrayList<Vector> s_copy= new ArrayList<>();
+        pointsRDD.forEachRemaining(s_copy::add);
+        ArrayList<Vector> centers= new ArrayList<>();
+        //Extracting a random point from the dataset as the initial center
+        int index=generator.nextInt(s_copy.size());
+        centers.add(s_copy.remove(index));
+        //initializing a data structure for keeping the minimum distance between
+        //a point not selected as center and the center subset
+        ArrayList<Double> centers_distances=new ArrayList<>();
+        for(int i=0; i<s_copy.size();i++){//O(|S|)
+            centers_distances.add(sqrt(Vectors.sqdist(s_copy.get(i),centers.get(0))));
+        }
+        //For cicle which extracts at each iteration the point with the maximum distance
+        //to the closest point of the center subset
+        //Total Cost: O(k*|S|)
+        for (int i=1;i<k;i++){//O(k)
+            double best_distance=-1;
+            int best_point=-1;
+            //Calculating the point which minimizes the distance with the center subset
+            for(int j=0;j<s_copy.size();j++){//O(|S|)
+                if (centers_distances.get(j)>best_distance){
+                    best_distance=centers_distances.get(j);
+                    best_point=j;
                 }
             }
-            Vector selected = points.remove(max);
-            // Remove point p that maximizes d(p, S) from points and add to set of centers S
-            S.add(selected);
-            // Remove it also from partition since it becomes a new center and we don't need anymore to know its
-            // distance from centers
-            partition.remove(selected);
+            Vector new_center=s_copy.remove(best_point);
+            centers.add(new_center);
+            centers_distances.remove(best_point);
+            //Updating the centers_distances data structure
+            for(int j=0;j<centers_distances.size();j++){//O(|S|)
+                double new_center_distance=sqrt(Vectors.sqdist(s_copy.get(j),new_center));
+                if(new_center_distance<centers_distances.get(j)){
+                    centers_distances.set(j,new_center_distance);
+                }
+            }
         }
-        HashMap<Vector, ArrayList<Vector>> res = new HashMap<>();
-        for (Vector c: S)
-            res.put(c, new ArrayList<>());
-        for(Map.Entry<Vector, Tuple2<Vector, Double>> point : partition.entrySet())
-            res.get(point.getValue()._1).add(point.getKey());
-        List<Tuple2<Vector, ArrayList<Vector>>> partitioned = new ArrayList<>();
-        for(Map.Entry<Vector, ArrayList<Vector>> p : res.entrySet())
-            partitioned.add(new Tuple2<>(p.getKey(), p.getValue()));
-        return sc.parallelizePairs(partitioned);
+
+        return centers.iterator();
     }
+
 
     public static Vector readPoint(String point) {
         return Vectors.dense(Arrays.stream(point.split(",")).mapToDouble(Double::parseDouble).toArray());
     }
 
 
-    public static void runMapReduce(JavaRDD<Object> pointsRDD, int k, int L) {
-        JavaRDD<Object> res = pointsRDD.mapPartitions(partition -> {
-            return farthestFirstTraversal(partition, k);
-        });
-        System.out.println("rr");
-        List<String> a = Arrays.asList("aa", "bb", "cc");
-        Iterator<String> b = a.iterator();
+    public static double measure(List<Vector> pointSet) {
+        double sum = 0;
+        for(Vector point1: pointSet)
+            for(Vector point2: pointSet)
+                sum += Vectors.sqdist(point1, point2);
+        double k = pointSet.size();
+        return sum / ((k * ( k - 1 )) / 2);
+    }
 
+    public static void runMapReduce(JavaRDD<Vector> pointsRDD, int k, int L) {
+        // Calculate time
+        long startTime = System.currentTimeMillis();
+        // Repartition the JavaRDD
+        JavaRDD<Vector> points = pointsRDD.repartition(L).cache();
+        System.out.println("Number of points = " + pointsRDD.collect().size() + "\nk = " + k + "\nL = " + L + "\nInitialization time = " + (System.currentTimeMillis() - startTime) + " ms" );
+        startTime = System.currentTimeMillis();
+        List<Vector> coreset = points.mapPartitions(partition -> farthestFirstTraversal(partition, k)).collect();
+        System.out.println("Runtime of Round 1 = " + (System.currentTimeMillis() - startTime) + " ms");
+        startTime = System.currentTimeMillis();
+        List<Vector> centers = runSequential(coreset, k);
+        System.out.println("Runtime of Round 2 = " + (System.currentTimeMillis() - startTime) + " ms\nAverage distance = " + measure(centers));
     }
 
     public static void main(String[] args) throws IOException {
@@ -98,15 +107,11 @@ public class G48HW3 {
             throw new IllegalArgumentException("USAGE: needed path, k, L.");
         }
         sc.setLogLevel("WARN");
-        // Read number of partitions
-
-        long startTime = System.currentTimeMillis();
+        // Read inputs
         String path = args[0];
         int k = Integer.parseInt(args[1]);
         int L = Integer.parseInt(args[2]);
-        JavaRDD<Vector> pointsRDD = sc.textFile(path).map(t -> readPoint(t)).repartition(L).cache();
-        long endTime = System.currentTimeMillis();
-        System.out.println("Number of points = " + pointsRDD.collect().size() + "\nk = " + k + "\nL = " + L + "\nInitialization time = " + (endTime - startTime) + " ms" );
+        JavaRDD<Vector> pointsRDD = sc.textFile(path).map(G48HW3::readPoint);
         runMapReduce(pointsRDD, k, L);
 
         //ArrayList<Vector> a = runSequential(readCSV(path), 10);
@@ -123,7 +128,7 @@ public class G48HW3 {
     // Sequential 2-approximation based on matching
     // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-    public static ArrayList<Vector> runSequential(final ArrayList<Vector> points, int k) {
+    public static List<Vector> runSequential(final List<Vector> points, int k) {
 
         final int n = points.size();
         if (k >= n) {
